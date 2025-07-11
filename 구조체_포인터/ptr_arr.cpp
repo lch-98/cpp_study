2b2ddbc5a2bf02001dd91920fe71a0b080ce3e59
lch_98 <dlcndgusgnss@naver.com>
2025-07-11 16:29:28 +0900

    Cpp Study Arr with ptr

--- 구조체_포인터/ptr_arr.cpp
+++ 구조체_포인터/ptr_arr.cpp
@@ -5,11 +5,14 @@ int main() {
     int arr[] = { 1, 2, 3 };
     int* ptr = arr;
 
-    cout << ptr << " " << ptr + 1 << endl;
-    cout << *(ptr + 0) << " " << *(ptr + 1) << endl;
-    cout << ptr[0] << " " << ptr[1] << endl;
-    cout << &arr[0] << " " << &arr[1] << endl;
-    cout << arr[0] << " " << arr[1] << endl;
+    cout << &ptr << endl; // 포인터의 주소
+    cout << ptr << " " << ptr + 1 << endl; // 포인터 가르키는 주소 == 배열 주소
+    cout << *(ptr + 0) << " " << *(ptr + 1) << endl; // 값
+    cout << ptr[0] << " " << ptr[1] << endl; // 값
 
+    cout << arr << " " << arr + 1 << endl; // 배열 주소
+    cout << &arr[0] << " " << &arr[1] << endl; // 배열 주소
+    cout << arr[0] << " " << arr[1] << endl; // 값
+    cout << *(&arr[0]) << " " << *(&arr[1]) << endl; // 값
     return 0;
 }
